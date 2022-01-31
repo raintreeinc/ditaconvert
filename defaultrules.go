bcfc95a9173ef54297476149b8837b8f2f3f23d6
Uwe Tallmeister <10596530+uwetallmeister@users.noreply.github.com>
2022-01-31 18:50:38 +0200

    Update defaultrules.go
    Removed Flash fallback from video tags

--- defaultrules.go
+++ defaultrules.go
@@ -202,10 +202,7 @@ func NewDefaultRules() *Rules {
 					const videof = `` +
 						`<video controls>` +
 						`	<source src="%s" type="video/mp4">` +
-						`	<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" ` +
-						`		<param name="SRC" value="http://ie.microsoft.com/testdrive/IEBlog/Common/player.swf?file=%s">` +
 						`		<p>Video playback not supported</p>` +
-						`	</object>` +
 						`</video>`
 
 					srcurl := html.NormalizeURL(href)
